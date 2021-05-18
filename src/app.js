1499540aa083aaafe5738ca2d20758eb44c567c9
Samira <samira.warsame1@gmail.com>
2021-05-18 00:38:37 -0400

    Correction on apiUrl

--- src/app.js
+++ src/app.js
@@ -73,7 +73,7 @@ function displayForecast(response) {
 function getForecast(coordinates) {
   console.log(coordinates);
   let apiKey = "99d192e05eead599ecd11f39a474da55";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&unit=metric`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
 }
 
